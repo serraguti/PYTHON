2a72a90a28ed3ad132de5e81d88d8df9d3dd4952
Serra <pacoserranox@gmail.com>
2021-10-22 17:58:24 +0200

    commit

--- mainnarcisista.py
+++ mainnarcisista.py
@@ -2,3 +2,15 @@ import metodosnarcisista
 
 print("Ejemplo narcisista y bisiesto")
 metodosnarcisista.menuNarcisista();
+opcion = int(input())
+if (opcion == 1):
+    print("Introduzca un número")
+    numero = int(input())
+    metodosnarcisista.isBisiesto(numero)
+elif (opcion == 2):
+    print("Introduzca un número")
+    textonumero = input()
+    metodosnarcisista.isNarcisista(textonumero)
+else:
+    print("Hasta luego")
+print("Fin de programa")
