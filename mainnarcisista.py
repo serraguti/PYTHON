8ba687f12d2885e231fbfe7e757d8a112467c487
Serra <pacoserranox@gmail.com>
2021-10-22 17:59:26 +0200

    vvv

--- mainnarcisista.py
+++ mainnarcisista.py
@@ -1,16 +1,18 @@
 import metodosnarcisista
 
 print("Ejemplo narcisista y bisiesto")
-metodosnarcisista.menuNarcisista();
-opcion = int(input())
-if (opcion == 1):
-    print("Introduzca un número")
-    numero = int(input())
-    metodosnarcisista.isBisiesto(numero)
-elif (opcion == 2):
-    print("Introduzca un número")
-    textonumero = input()
-    metodosnarcisista.isNarcisista(textonumero)
-else:
-    print("Hasta luego")
+opcion = -1
+while (opcion != 0):
+    metodosnarcisista.menuNarcisista();
+    opcion = int(input())
+    if (opcion == 1):
+        print("Introduzca un número")
+        numero = int(input())
+        metodosnarcisista.isBisiesto(numero)
+    elif (opcion == 2):
+        print("Introduzca un número")
+        textonumero = input()
+        metodosnarcisista.isNarcisista(textonumero)
+    else:
+        print("Hasta luego")
 print("Fin de programa")
